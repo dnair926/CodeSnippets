070dbfaab1cd98431773a8f745031853641c13e6
Deleep Nair <dnair926@aol.com>
2014-05-22 18:38:21 -0400

    Initial Commit

---
+++ Samples/DisposableSealedObject.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+
+namespace CodeSnippets.Samples {
+    public class DisposableSealedObject : IDisposable {
+
+        #region IDisposable Members
+
+        private bool _isDisposed = false;
+
+        ~DisposableSealedObject() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool canDisposeManagedResources) {
+            if (_isDisposed) {
+				return;			
+            }
+
+            if (canDisposeManagedResources) {
+                try {
+					DisposeManagedResources();
+                } catch (Exception ex) {
+
+                }
+            }
+
+            try {
+				DisposeUnmanagedResources();
+            } catch (Exception ex) {
+
+            }
+
+            _isDisposed = true;
+        }
+
+		private void DisposeManagedResources() {
+			
+
+		}
+    
+		private void DisposeUnmanagedResources() {
+    
+		}
+    
+		/// <summary>
+		/// This should be in all public functions and properties as the first call.
+		/// </summary>
+        private void CheckIfDisposeAndRaiseException() {
+            if (_isDisposed)
+                throw new ObjectDisposedException(@"BothTypeOfResourcesClass");
+
+        }
+
+        #endregion
+
+              
+    }
+}
